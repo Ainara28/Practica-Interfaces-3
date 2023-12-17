51a4daf8506b1b284a6643f3c094831d74bf679e
ainara28 <ainara2830@gmail.com>
2023-12-17 21:39:46 +0100

    actualizacion

--- script/personalizado.js
+++ script/personalizado.js
@@ -74,4 +74,41 @@ $(document).ready(function () {
       container.find('button').css('display', 'none');
       $('#uploadButton').val(''); // Limpiamos el valor del input file para permitir cargar la misma imagen nuevamente
     };
+
+    // Agrega un manejador de eventos para el botón "Enviar"
+    $('.enviar').on('click', function () {
+      // Valida que todos los campos estén llenos y que se haya subido una imagen
+      if (validarCampos() && validarImagen()) {
+          // Muestra la alerta
+          alert('Su pedido ha sido realizado exitosamente ¡Gracias!');
+
+          // Redirige a la página home.html
+          window.location.href = 'home.html';
+      } else {
+          // Si algún campo está vacío o no se ha subido una imagen, muestra un mensaje de error
+          alert('Por favor, complete todos los campos y suba una imagen antes de enviar el pedido.');
+      }
+  });
+  function validarCampos() {
+    // Obtiene los valores de los campos
+    var nombreDiseno = $('#diseño').val();
+    var tamaño = $('#tamaño').val();
+    var colores = $('#colores').val();
+    var descripcion = $('#descripcion').val();
+
+    // Verifica que todos los campos tengan contenido
+    if (nombreDiseno.trim() === '' || tamaño.trim() === '' || colores.trim() === '' || descripcion.trim() === '') {
+        return false; // Al menos un campo está vacío
+    }
+
+    // Si todos los campos tienen contenido, retorna true
+    return true;
+}
+  function validarImagen() {
+    // Obtiene la ruta de la imagen
+    var rutaImagen = $('.imagen img').attr('src');
+
+    // Verifica que se haya subido una imagen
+    return rutaImagen !== 'images/upload.png';
+  }
   });
